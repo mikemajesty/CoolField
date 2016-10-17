afdb6906eed592196138f16282eb42fd999a8a93
mikemajesty <mikemajesty2011@gmail.com>
2016-10-17 20:14:59 -0200

    Add project files.

---
+++ CoolField/CoolField.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoolField
+{
+    public static class CoolField
+    {
+        public static string UpperCaseOnlyFirst(this TextBox txt, int mininumSize = 1)
+        {
+            return txt.Text.Length >= mininumSize ? txt.Text.First().ToString().ToUpper() + String.Join("", txt.Text.Skip(1)).ToLower() : "";
+        }
+   
+        public static string UpperCaseAll(this TextBox txt, int mininumSize = 1)
+        {
+            return txt.Text.Length >= mininumSize ? txt.Text.ToString().ToUpper() : "";
+        }
+        
+        public static string LowerCaseAll(this TextBox txt, int mininumSize = 1)
+        {
+            return txt.Text.Length >= mininumSize ? txt.Text.ToString().ToLower() : "";
+        }
+    }
+}
