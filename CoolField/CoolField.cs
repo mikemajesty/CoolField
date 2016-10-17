80147bdf63c73a94d5631ec16ff100db3b1a3523
mikemajesty <mikemajesty2011@gmail.com>
2016-10-17 20:19:28 -0200

    Remove spaces

--- CoolField/CoolField.cs
+++ CoolField/CoolField.cs
@@ -10,12 +10,12 @@ namespace CoolField
         {
             return txt.Text.Length >= mininumSize ? txt.Text.First().ToString().ToUpper() + String.Join("", txt.Text.Skip(1)).ToLower() : "";
         }
-   
+
         public static string UpperCaseAll(this TextBox txt, int mininumSize = 1)
         {
             return txt.Text.Length >= mininumSize ? txt.Text.ToString().ToUpper() : "";
         }
-        
+
         public static string LowerCaseAll(this TextBox txt, int mininumSize = 1)
         {
             return txt.Text.Length >= mininumSize ? txt.Text.ToString().ToLower() : "";
