ae091e68376dbfba815446f9e66e9c6532c21cd9
mikemajesty <mikemajesty2011@gmail.com>
2016-10-17 20:45:50 -0200

    Change methods name

--- CoolField/CoolField.cs
+++ CoolField/CoolField.cs
@@ -7,17 +7,17 @@ namespace CoolField
     public static class CoolField
     {
 
-        public static string ToUpperCaseOnlyFirst(this TextBox txt, int mininumSize = 1)
+        public static string UpperCaseOnlyFirst(this TextBox txt, int mininumSize = 1)
         {
             return txt.Text.Length >= mininumSize ? txt.Text.First().ToString().ToUpper() + String.Join("", txt.Text.Skip(1)).ToLower() : "";
         }
 
-        public static string ToUpperCaseAll(this TextBox txt, int mininumSize = 1)
+        public static string UpperCaseAll(this TextBox txt, int mininumSize = 1)
         {
             return txt.Text.Length >= mininumSize ? txt.Text.ToString().ToUpper() : "";
         }
 
-        public static string ToLowerCaseAll(this TextBox txt, int mininumSize = 1)
+        public static string LowerCaseAll(this TextBox txt, int mininumSize = 1)
         {
             return txt.Text.Length >= mininumSize ? txt.Text.ToString().ToLower() : "";
         }
